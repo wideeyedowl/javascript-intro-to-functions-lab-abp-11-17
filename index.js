18487f618b7462c0bb85264feed7afee3cac6595
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-16 02:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase())
       {
-        return "I can't hear you";
+        return "I can't hear you!";
       }
    if (string === string.toUpperCase())
       {
-     return "YES INDEED";
+     return "YES INDEED!";
       }
-  if (string ==="I love you, Grandma")
+  if (string ==="I love you, Grandma.")
       {
-        return "I love you, too";
+        return "I love you, too.";
       }
 }
