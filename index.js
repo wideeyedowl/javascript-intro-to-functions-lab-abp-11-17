dac0b420f9363df677935f6ad6a3917803765491
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-16 02:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,11 @@ function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase())
       {
-        return "I can\'t hear you";
+        return "I can't hear you";
       }
-}
- function sayHiToGrandma(string)
- {
    if (string === string.toUpperCase())
    {
      return "YES INDEED";
    }
  }
+ 
