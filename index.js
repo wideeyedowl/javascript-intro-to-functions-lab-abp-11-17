9624afe21ba0091186b53d743fefe580092ac9f1
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-16 02:01:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ function logShout(string)
  {
    console.log(string.toLowerCase());
  }
+function sayHiToGrandma(string)
+{
+  if (string === string.toLowerCase())
+      {
+        return "I can\'t hear you";
+      }
+}
+ function sayHiToGrandma(string)
+ {
+   if (string === string.toUpperCase())
+   {
+     return "YES INDEED";
+   }
+ }
